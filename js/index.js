2ee45611ac759f457bbb548893677a37d60a7e9f
Kim Seung-yeop <a01055143647@gmail.com>
2024-05-27 22:10:29 +0900

    fix: index.html 주문

--- js/index.js
+++ js/index.js
@@ -135,7 +135,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         orders.forEach((order) => {
           var card = document.createElement("div");
-          card.className = "col-xl-3 col-md-6 mb-4";
+          card.className = "col-12 mb-4";
           card.innerHTML = `
             <div class="card border-left-primary shadow h-100 py-2">
               <div class="card-body">
